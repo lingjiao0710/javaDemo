631f7451982afb8667c3d3fc066ec6200221c0ea
Flj <Flj@Flj-Desktop>
2018-11-15 21:05:58 +0800

    FEATURE 增加反射

---
+++ Reflection/ReflectionDemo.java
@@ -0,0 +1,33 @@
+package com.flj.reflection;
+
+public class ReflectionDemo {
+	
+	public static void main(String[] args) throws Exception {
+		//三种方式
+		//1.对象.getClass()
+		@SuppressWarnings("rawtypes")
+		//Class cls = new Iphone().getClass();
+		
+		//2.类.class()
+		//cls = Iphone.class;
+		
+		//3.Class.forName("包名.类名")
+		Class cls = Class.forName("com.flj.reflection.Iphone");
+		
+		//创建对象
+		//不推荐这种方式
+		//Iphone i2 = (Iphone) cls.newInstance();
+		
+		//推荐
+		Iphone i2 = (Iphone) cls.getConstructor().newInstance();
+		System.out.println(i2);
+	}
+	
+	
+}
+
+class Iphone{
+	public Iphone() {
+		
+	}
+}
